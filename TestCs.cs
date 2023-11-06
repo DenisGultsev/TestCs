4d7c2012c71679e6e157699b5fa25259b3966458
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-11-07 00:30:37 +1000

    Program

---
+++ TestCs.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите строки через запятую:");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(',');
+
+int count = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Trim().Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] resultArray = new string[count];
+int index = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Trim().Length <= 3)
+    {
+        resultArray[index] = inputArray[i].Trim();
+        index++;
+    }
+}
+
+Console.Write("[");
+for (int i = 0; i < resultArray.Length; i++)
+{
+    Console.Write("\"" + resultArray[i] + "\"");
+    if (i < resultArray.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine("]");
